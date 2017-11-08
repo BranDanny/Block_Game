d67fc3331532538efe0cc7f574a355ddc3045fd2
BranDanny <zhangdeng1010@163.com>
2017-11-08 17:59:47 +0800

    fix

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 $(function () {
-
     //存放基础数据
     var data = {
         top: 100,
@@ -60,15 +59,38 @@ $(function () {
             clickedBlock.isDrag = true;
             mouseInitX = e.pageX - clickedBlock.left;
             mouseInitY = e.pageY - clickedBlock.top;
+             // view.render();
+        },
+        //移动下边框位置
+        Bdown: function (e, block) {
+            var clickedBlock = data.blocks[block.id - 1];
+            clickedBlock.BisDrag = true;
+            BInitY = e.pageY - clickedBlock.resizeBTop;
             // view.render();
         },
 
-        mousemoveBlock: function (e, block) {
+        //移动右边框位置
+        Rdown: function (e, block) {
             var clickedBlock = data.blocks[block.id - 1];
-            if (clickedBlock.isDrag === true) {
-                var moveX = e.pageX - mouseInitX;
-                var moveY = e.pageY - mouseInitX;
+            clickedBlock.RisDrag = true;
+            RInitX = e.pageX - clickedBlock.resizeRLeft;
+            // view.render();
+        },
 
+        //移动右下小方块位置
+        RBdown: function (e, block) {
+            var clickedBlock = data.blocks[block.id - 1];
+            clickedBlock.RBisDrag = true;
+            RBInitX = e.pageX - clickedBlock.resizeRBLeft;
+            RBInitY = e.pageY - clickedBlock.resizeRBTop;
+            // view.render();
+        },
+
+        move: function (e, block) {
+            var clickedBlock = data.blocks[block.id - 1];
+            if(clickedBlock.isDrag === true){
+                var moveX = e.pageX - mouseInitX;
+                var moveY = e.pageY - mouseInitY;
                 var Xmax = $(window).width() - clickedBlock.width;
                 var Ymax = $(window).height() - clickedBlock.height;
 
@@ -85,90 +107,43 @@ $(function () {
                 clickedBlock.resizeRBLeft = clickedBlock.left + clickedBlock.width - 20;
 
                 view.render();
-            }
-        },
 
-        mouseupBlock: function (e, block) {
-            var clickedBlock = data.blocks[block.id - 1];
-            clickedBlock.isDrag = false;
-            view.render()
-        },
+            }else if (clickedBlock.RisDrag === true) {
+                var RX = e.pageX - RInitX;
+                var RXmax = $(window).width() - 10;
 
-        //移动右边框位置
-        Rdown: function (e, block) {
-            var clickedBlock = data.blocks[block.id - 1];
-            clickedBlock.RisDrag = true;
-            mouseInitX = e.pageX - clickedBlock.resizeRLeft;
-            // view.render();
-        },
+                clickedBlock.resizeRLeft = Math.min(RXmax, Math.max(0, RX));
 
-        Rmove: function (e, block) {
-            var clickedBlock = data.blocks[block.id - 1];
-            if (clickedBlock.RisDrag === true) {
-                var moveX = e.pageX - mouseInitX;
-                var Xmax = $(window).width() - clickedBlock.width;
-                clickedBlock.resizeRLeft = Math.min(Xmax, Math.max(0, moveX));
                 clickedBlock.width = Math.max(50, clickedBlock.resizeRLeft - clickedBlock.left + 10);
+
                 clickedBlock.resizeRBLeft = clickedBlock.left + clickedBlock.width - 20;
+
                 view.render();
-            }
-        },
 
-        Rup: function (e, block) {
-            var clickedBlock = data.blocks[block.id - 1];
-            clickedBlock.RisDrag = false;
-            view.render()
-        },
+            }else if(clickedBlock.BisDrag === true){
+                var BY = e.pageY - BInitY;
+                var BYmax = $(window).height() - 10;
 
-        //移动下边框位置
-        Bdown: function (e, block) {
-            var clickedBlock = data.blocks[block.id - 1];
-            clickedBlock.BisDrag = true;
-            mouseInitY = e.pageY - clickedBlock.resizeBTop;
-            // view.render();
-        },
+                clickedBlock.resizeBTop = Math.min(BYmax, Math.max(0, BY));
 
-        Bmove: function (e, block) {
-            var clickedBlock = data.blocks[block.id - 1];
-            if (clickedBlock.BisDrag === true) {
-                var moveY = e.pageY - mouseInitY;
-                var Ymax = $(window).height() - clickedBlock.height;
-                clickedBlock.resizeBTop = Math.min(Ymax, Math.max(0, moveY));
                 clickedBlock.height = Math.max(50, clickedBlock.resizeBTop - clickedBlock.top + 10);
+
                 clickedBlock.resizeRBTop = clickedBlock.top + clickedBlock.height - 20;
+
                 view.render();
-            }
-        },
 
-        Bup: function (e, block) {
-            var clickedBlock = data.blocks[block.id - 1];
-            clickedBlock.BisDrag = false;
-            view.render()
-        },
+            }else if(clickedBlock.RBisDrag === true){
+                var RBX = e.pageX - RBInitX;
+                var RBY = e.pageY - RBInitY;
+                var RBXmax = $(window).width() - 20;
+                var RBYmax = $(window).height() - 20;
 
-        //移动右下小方块位置
-        RBdown: function (e, block) {
-            var clickedBlock = data.blocks[block.id - 1];
-            clickedBlock.RBisDrag = true;
-            mouseInitX = e.pageX - clickedBlock.resizeRBLeft;
-            mouseInitY = e.pageY - clickedBlock.resizeRBTop;
-            // view.render();
-        },
-
-        RBmove: function (e, block) {
-            var clickedBlock = data.blocks[block.id - 1];
-            if (clickedBlock.RBisDrag === true) {
-                var moveX = e.pageX - mouseInitX;
-                var moveY = e.pageY - mouseInitX;
-
-                var Xmax = $(window).width() - clickedBlock.width;
-                var Ymax = $(window).height() - clickedBlock.height;
-
-                clickedBlock.resizeRBTop = Math.min(Ymax, Math.max(0, moveY));
-                clickedBlock.resizeRBLeft = Math.min(Xmax, Math.max(0, moveX));
+                clickedBlock.resizeRBTop = Math.min(RBYmax, Math.max(0, RBY));
+                clickedBlock.resizeRBLeft = Math.min(RBXmax, Math.max(0, RBX));
 
                 clickedBlock.width = Math.max(50, clickedBlock.resizeRBLeft - clickedBlock.left + 20);
                 clickedBlock.height = Math.max(50, clickedBlock.resizeRBTop - clickedBlock.top + 20);
+
                 clickedBlock.resizeRLeft = clickedBlock.left + clickedBlock.width - 10;
                 clickedBlock.resizeBTop = clickedBlock.top + clickedBlock.height - 10;
 
@@ -176,9 +151,13 @@ $(function () {
             }
         },
 
-        RBup: function (e, block) {
+        up: function (e, block) {
             var clickedBlock = data.blocks[block.id - 1];
+            clickedBlock.isDrag = false;
+            clickedBlock.RisDrag = false;
+            clickedBlock.BisDrag = false;
             clickedBlock.RBisDrag = false;
+
             view.render()
         },
 
@@ -208,40 +187,34 @@ $(function () {
             this.blockList.on('mousedown', '.remove-block', function (e) {
                 e.stopPropagation();
                 var block = $(this).parent('.block').data();
+                console.log(block);
                 octopus.removeBlock(block);
                 return false;
             }); //删除大方块
 
-            this.blockList.on({
-                mousedown: function (e) {
-                    var block = $(this).data();
-                    octopus.mousedownBlock(e, block);
-                },
-                mousemove: function (e) {
-                    var block = $(this).data();
-                    octopus.mousemoveBlock(e, block);
-                },
-                mouseup: function (e) {
-                    var block = $(this).data();
-                    octopus.mouseupBlock(e, block);
-                }
-            }, '.block');//移动大方块
+
+            this.blockList.on('mousedown','.block', function (e) {
+                var block = $(this).data();
+                octopus.mousedownBlock(e, block);
+                $(document).mousemove(function (e) {
+                    octopus.move(e, block);
+                });
+                $(document).mouseup(function (e) {
+                    octopus.up(e, block);
+                })
+            }); //移动大方块
 
             this.blockList.on({
                 mousedown: function (e) {
                     e.stopPropagation();
                     var block = $(this).parent('.block').data();
                     octopus.Rdown(e, block);
-                },
-                mousemove: function (e) {
-                    e.stopPropagation();
-                    var block = $(this).parent('.block').data();
-                    octopus.Rmove(e, block);
-                },
-                mouseup: function (e) {
-                    e.stopPropagation();
-                    var block = $(this).parent('.block').data();
-                    octopus.Rup(e, block);
+                    $(document).mousemove(function (e) {
+                        octopus.move(e, block);
+                    });
+                    $(document).mouseup(function (e) {
+                        octopus.up(e, block);
+                    })
                 }
             }, '.resizeR');//移动右边框
 
@@ -250,16 +223,12 @@ $(function () {
                     e.stopPropagation();
                     var block = $(this).parent('.block').data();
                     octopus.Bdown(e, block);
-                },
-                mousemove: function (e) {
-                    e.stopPropagation();
-                    var block = $(this).parent('.block').data();
-                    octopus.Bmove(e, block);
-                },
-                mouseup: function (e) {
-                    e.stopPropagation();
-                    var block = $(this).parent('.block').data();
-                    octopus.Bup(e, block);
+                    $(document).mousemove(function (e) {
+                        octopus.move(e, block);
+                    });
+                    $(document).mouseup(function (e) {
+                        octopus.up(e, block);
+                    })
                 }
             }, '.resizeB');//移动下边框
 
@@ -268,23 +237,19 @@ $(function () {
                     e.stopPropagation();
                     var block = $(this).parent('.block').data();
                     octopus.RBdown(e, block);
-                },
-                mousemove: function (e) {
-                    e.stopPropagation();
-                    var block = $(this).parent('.block').data();
-                    octopus.RBmove(e, block);
-                },
-                mouseup: function (e) {
-                    e.stopPropagation();
-                    var block = $(this).parent('.block').data();
-                    octopus.RBup(e, block);
+                    $(document).mousemove(function (e) {
+                        octopus.move(e, block);
+                    });
+                    $(document).mouseup(function (e) {
+                        octopus.up(e, block);
+                    })
                 }
             }, '.resizeRB');//移动右下小方块
 
             this.render();
         },
 
-        render: function () {
+        render: _.debounce(function () {
             // Cache vars for use in forEach() callback (performance)
             var blockList = this.blockList,
                 blockTemplate = this.blockTemplate;
@@ -297,12 +262,12 @@ $(function () {
                 blockList.append(thisTemplate);
 
                 $("div[data-id=" + block.id + "]").offset({top: block.top, left: block.left})
-                                                  .css({'width': block.width + 'px', 'height': block.height + 'px', 'background': block.ranColor});
+                    .css({'width': block.width + 'px', 'height': block.height + 'px', 'background': block.ranColor});
                 $("div[data-id=" + block.id + "] > .resizeR").offset({top: block.resizeRTop, left: block.resizeRLeft});
                 $("div[data-id=" + block.id + "] > .resizeB").offset({top: block.resizeBTop, left: block.resizeBLeft});
                 $("div[data-id=" + block.id + "] > .resizeRB").offset({top: block.resizeRBTop, left: block.resizeRBLeft});
             });
-        }
+        },50)
     };
 
     octopus.init();
